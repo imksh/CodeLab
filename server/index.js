05f32a344346311a7b646617718a901638253688
Karan <karan03945@gmail.com>
2026-06-24 01:22:16 +0530

    cors updated client url

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://codelab-imksh.netlify.app", "https://codelab.imksh.online"],
+    origin: ["http://localhost:5173","https://codelab-imksh.netlify.app", "https://codelab.imksh.online",process.env.CLIENT_URL],
     credentials: true,
   }),
 );
